6e41d4c5ee429a1b167e3dbcd5aa160414b8ba11
Uday Gohel <goheluday6445@gmail.com>
2023-08-08 00:05:14 +0530

    ErrorModal Added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react"; // Not Required
+import React, { useRef, useState } from "react"; // Not Required
 import "./ExpenseForm.css";
+import ErrorModal from "../UI/ErrorModal";
 
 const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
@@ -13,91 +14,151 @@ const ExpenseForm = (props) => {
   //   enteredDate: "",
   // });
 
-  const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+  // const titleChangeHandler = (event) => {
+  //   setEnteredTitle(event.target.value);
 
-    // OR TRY THIS NOT PROPER WAY TO USE THIS
-    // setUserInput({
-    //   ...userInput, // overwrite thay che value
-    //   enteredTitle: event.target.value,
-    // });
+  // OR TRY THIS NOT PROPER WAY TO USE THIS
+  // setUserInput({
+  //   ...userInput, // overwrite thay che value
+  //   enteredTitle: event.target.value,
+  // });
 
-    // This is the best way if we update counter or in react scheduling this will return new state in efficient way
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enteredTitle: event.target.value };
-    // });
-  };
+  // This is the best way if we update counter or in react scheduling this will return new state in efficient way
+  // setUserInput((prevState) => {
+  //   return { ...prevState, enteredTitle: event.target.value };
+  // });
+  // };
 
-  const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredAmount: event.target.value,
-    // });
-  };
+  // const amountChangeHandler = (event) => {
+  //   setEnteredAmount(event.target.value);
+  // setUserInput({
+  //   ...userInput,
+  //   enteredAmount: event.target.value,
+  // });
+  // };
 
-  const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: event.target.value,
-    // });
-  };
+  // const dateChangeHandler = (event) => {
+  //   setEnteredDate(event.target.value);
+  // setUserInput({
+  //   ...userInput,
+  //   enteredDate: event.target.value,
+  // });
+  // };
+  const titleInputRef = useRef();
+  const amountInputRef = useRef();
+  const dateInputRef = useRef();
+
+  const [error, setError] = useState();
 
   const submitHandler = (event) => {
     event.preventDefault();
 
+    const titleRef = titleInputRef.current.value;
+    const amountRef = amountInputRef.current.value;
+    const dateRef = dateInputRef.current.value;
+
+    if (titleRef.trim().length === 0 || amountRef.trim().length === 0) {
+      setError({
+        title: "Invalid Input",
+        message:
+          "Please enter a valid Input !! Amount and Title Can not be Empty ",
+      });
+      return;
+    }
+    if (+amountRef < 1) {
+      setError({
+        title: "Invalid Amount",
+        message: "Amount must be Grater than 0  (non-empty values).",
+      });
+      return;
+    }
+    console.log(titleInputRef);
+
+    const date = new Date();
+    const date2 = new Date(dateRef);
+    if (date2 > date) {
+      setError({
+        title: "Invalid Date",
+        message: "Date must be Previous or Today Date   (non-empty values).",
+      });
+      return;
+    }
+
     const expenseDate = {
-      title: enteredTitle,
-      amount: +enteredAmount,
-      date: new Date(enteredDate),
+      title: titleRef,
+      amount: +amountRef,
+      date: new Date(dateRef),
     };
+    setEnteredTitle(titleRef);
+    setEnteredAmount(amountRef);
+    setEnteredDate(dateRef);
 
     props.onSaveExpenseData(expenseDate);
-    setEnteredTitle("");
-    setEnteredDate("");
-    setEnteredAmount("");
+    titleInputRef.current.value = "";
+    amountInputRef.current.value = "";
+    // setEnteredTitle("");
+    // setEnteredDate("");
+    // setEnteredAmount("");
+  };
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+    <React.Fragment>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label htmlFor="title">Title</label>
+            <input
+              id="title"
+              type="text"
+              // value={enteredTitle}
+              // onChange={titleChangeHandler}
+              ref={titleInputRef}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label htmlFor="amount">Amount</label>
+            <input
+              id="amount"
+              type="text"
+              min="0.01"
+              step="0.01"
+              // value={enteredAmount}
+              // onChange={amountChangeHandler}
+              ref={amountInputRef}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label htmlFor="date">Date</label>
+            <input
+              id="date"
+              type="date"
+              min="2022-01-01"
+              max="2023-12-31"
+              // value={enteredDate}
+              // onChange={dateChangeHandler}
+              ref={dateInputRef}
+            />
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="text"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
+        <div className="new-expense__actions">
+          <button type="button" onClick={props.onCancel}>
+            Cancel
+          </button>
+          <button type="submit">Add Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2022-01-01"
-            max="2023-12-31"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="button" onClick={props.onCancel}>
-          Cancel
-        </button>
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+      </form>
+    </React.Fragment>
   );
 };
 
